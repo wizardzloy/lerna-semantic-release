a81af484ec7514816ffe4ae2a1eaf5afd94908cf
Joshua Nelson <jonelson@atlassian.com>
2016-10-24 16:04:51 +1100

    fix(utils): make the logging util universal
    affects: lerna-semantic-release-utils
    
    this is so that lerna-semantic-release-analyze-commits can become universal

--- packages/lerna-semantic-release-utils/log.js
+++ packages/lerna-semantic-release-utils/log.js
@@ -1,17 +1,24 @@
-var winston = require('winston');
+if (typeof window.console === 'undefined') {
+  var winston = require('winston');
 
-//
-// Configure CLI output on the default logger
-//
-winston.cli();
+  //
+  // Configure CLI output on the default logger
+  //
+  winston.cli();
 
-//
-// Configure CLI on an instance of winston.Logger
-//
-var logger = new winston.Logger({
-  transports: [
-    new (winston.transports.Console)()
-  ]
-});
-
-module.exports = logger.cli();
+  //
+  // Configure CLI on an instance of winston.Logger
+  //
+  var logger = new winston.Logger({
+    transports: [
+      new (winston.transports.Console)()
+    ]
+  });
+} else {
+  module.exports = {
+    error: console.error,
+    warn: console.warn,
+    log: console.log,
+    info: console.log,
+  };
+};
