5f039c27c8cf5bfc50f21c1487521d316877dbde
Joshua Nelson <jonelson@atlassian.com>
2017-01-29 12:54:38 -0600

    fix(publish): publish now respects access: public for scoped packages
    affects: lerna-semantic-release-io
    
    BREAKING CHANGE: io.npm.publish now requires an extra paramter - pkg, the package that is being
    
    published

--- packages/lerna-semantic-release-io/io/npm.js
+++ packages/lerna-semantic-release-io/io/npm.js
@@ -1,7 +1,13 @@
 var execAsTask = require('lerna-semantic-release-utils').execAsTask;
 
 module.exports = {
-  publish: function version (path, scopedPublicPackage) {
+  publish: function version (path, pkg) {
+    var isScopedPublicPackage = false ;
+    if (pkg.name.charAt(0)=='@' && pkg.config ) {
+      if (pkg.config.access) {
+        scopedPublicPackage = (pkg.config.access=='public');
+      }
+    }
     return execAsTask('npm publish ' + (scopedPublicPackage ? '--access=public ' :'') + path);
   },
   version: function version (v) {
