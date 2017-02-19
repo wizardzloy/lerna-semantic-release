82a55ff3d5ecb3d3697ad64398310a5829e20c53
Vladimir Guguiev <wizardzloy@gmail.com>
2017-02-19 13:29:58 +0100

    refactor(cli): allow cli to accept flags
    affects: lerna-semantic-release
    
    also print --help message with usage documentation

--- packages/lerna-semantic-release/bin/lerna-semantic-release.js
+++ packages/lerna-semantic-release/bin/lerna-semantic-release.js
@@ -1,8 +1,29 @@
 #!/usr/bin/env node
 
-var task = process.argv[2];
+var meow = require('meow');
 var io = require('lerna-semantic-release-io').default;
 var log = require('lerna-semantic-release-utils').log;
+
+var cli = meow(
+  {
+    help: `
+      Usage
+        $ lerna-semantic-release [command]
+        
+      Commands: 
+        pre       Set up the versions, tags and commits
+        perform   Publishes to npm
+        post      Generates a changelog in each package in a file named CHANGELOG.md - will not commit or push that file. If you want to do something with it, you will need to do this manually.
+    `,
+  },
+  {
+    alias: {
+      h: 'help'
+    }
+  }
+);
+
+var task = cli.input && cli.input[0];
 
 var tasks = {
   pre: require('lerna-semantic-release-pre'),
@@ -17,6 +38,7 @@ function errorHandler(err) {
 
 try {
   tasks[task]({
+    flags: cli.flags,
     io: io,
     callback: errorHandler
   });
